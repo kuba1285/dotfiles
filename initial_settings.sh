6593cb94cdbea4087aeb802435ae059bd58e35d6
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2023-10-24 04:16:42 +0900

    Update initial_settings.sh

--- initial_settings.sh
+++ initial_settings.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+env_lines="GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx"
+
+echo -e "$env_lines" >> /etc/environment
+
 chrome_flags="--force-dark-mode --enable-features=WebUIDarkMode --ozone-platform-hint=auto --enable-features=TouchpadOverscrollHistoryNavigation --gtk-version=4"
 
-echo "$chrome_flags" >> /opt/google/chrome/google-chrome
+echo "$chrome_flags" >> ~/opt/google/chrome/google-chrome
