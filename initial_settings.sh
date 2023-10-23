1b0703f34f894c3a5f8c9d490d6cce7f8436d857
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2023-10-24 04:21:15 +0900

    Update initial_settings.sh

--- initial_settings.sh
+++ initial_settings.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+nw_lines="IPv6PrivacyExtensions=true\nIgnoreCarrierLoss=3s"
+
+echo -e "$nw_lines" >> /etc/systemd/network/10-wlan0.network
+
 env_lines="GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx"
 
 echo -e "$env_lines" >> /etc/environment
