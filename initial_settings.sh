1a65fac83d5ef492f94aef32ade98452ae6b6607
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2023-10-24 04:22:41 +0900

    Update initial_settings.sh

--- initial_settings.sh
+++ initial_settings.sh
@@ -11,3 +11,5 @@ echo -e "$env_lines" >> /etc/environment
 chrome_flags="--force-dark-mode --enable-features=WebUIDarkMode --ozone-platform-hint=auto --enable-features=TouchpadOverscrollHistoryNavigation --gtk-version=4"
 
 echo "$chrome_flags" >> ~/opt/google/chrome/google-chrome
+
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
