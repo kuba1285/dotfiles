6c0416f9a81b1a8160c873e403d4ea734958ee49
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2023-10-27 01:58:57 +0900

    Update init_inst.sh

--- init_inst.sh
+++ init_inst.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+systemctl enable iwd bluetooth systemd-timesyncd systemd-networkd systemd-resolved --now
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -13,17 +16,11 @@ cd wf-install
 ./install.sh --prefix /opt/wayfire --stream master
 
 nw_lines="IPv6PrivacyExtensions=true\nIgnoreCarrierLoss=3s"
-
-echo -e "$nw_lines" >> /etc/systemd/network/10-wlan0.network
-
 env_lines="GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx"
-
-echo -e "$env_lines" >> /etc/environment
-
 chrome_flags="--force-dark-mode --enable-features=WebUIDarkMode --ozone-platform-hint=auto --enable-features=TouchpadOverscrollHistoryNavigation --gtk-version=4"
 
+echo -e "$env_lines" >> /etc/environment
+echo -e "$nw_lines" >> /etc/systemd/network/10-wlan0.network
 echo "$chrome_flags" >> /opt/google/chrome/google-chrome
 
-ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
-
-sudo pacman -S wget man-db waybar pavucontrol alsa-utils neofetch gtk4 dconf meson cmake seatd wl-clipboard ttf-font-awesome 
+sudo pacman -S wget man-db waybar pavucontrol alsa-utils neofetch gtk4 dconf meson cmake seatd wl-clipboard ttf-font-awesome
