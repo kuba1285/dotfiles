d6b3a74c5fb2493633e023a13de0751ea36626bb
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2023-10-30 19:04:15 +0900

    Update init_inst.sh

--- init_inst.sh
+++ init_inst.sh
@@ -28,6 +28,6 @@ nw_lines="IPv6PrivacyExtensions=true\nIgnoreCarrierLoss=3s"
 env_lines="GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx"
 chrome_flags="--force-dark-mode --enable-features=WebUIDarkMode --ozone-platform-hint=auto --enable-features=TouchpadOverscrollHistoryNavigation --gtk-version=4"
 
-echo -e "$env_lines" >> /etc/environment
-echo -e "$nw_lines" >> /etc/systemd/network/10-wlan0.network
-echo "$chrome_flags" >> /opt/google/chrome/google-chrome
+sudo echo -e "$env_lines" >> /etc/environment
+sudo echo -e "$nw_lines" >> /etc/systemd/network/10-wlan0.network
+sudo echo "$chrome_flags" >> /opt/google/chrome/google-chrome
