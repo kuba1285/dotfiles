502c76683bdf0fdc719b1f0aae7d0b7ff8de730b
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2023-11-02 01:36:18 +0900

    Update init_inst.sh

--- init_inst.sh
+++ init_inst.sh
@@ -35,11 +35,13 @@ cd wf-install &&
 
 env_lines="GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx"
 nw_lines="IPv6PrivacyExtensions=true\nIgnoreCarrierLoss=3s"
-chrome_flags="--force-dark-mode --enable-features=WebUIDarkMode --ozone-platform-hint=auto --enable-features=TouchpadOverscrollHistoryNavigation --gtk-version=4"
 
 sudo echo -e "$env_lines" | sudo tee -a /etc/environment
 sudo echo -e "$nw_lines" | sudo tee -a /etc/systemd/network/10-wlan0.network
-sudo echo "$chrome_flags" | sudo tee -a /opt/google/chrome/google-chrome
+
+# also available in .config/chrome-flags.conf
+#chrome_flags="--force-dark-mode --enable-features=WebUIDarkMode --ozone-platform-hint=auto --enable-features=TouchpadOverscrollHistoryNavigation --gtk-version=4"
+#sudo echo "$chrome_flags" | sudo tee -a /opt/google/chrome/google-chrome
 
 # wayfire software cursor
 sc_lines="WLR_NO_HARDWARE_CURSORS=1"
