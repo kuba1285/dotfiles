b9ba0c65f201a402c23fe25b5f2bf2a5298c42c1
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2023-11-04 03:18:34 +0900

    Update init_inst.sh

--- init_inst.sh
+++ init_inst.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 cd
-# update list
+# update and install general pkg
 sudo pacman -Syu
-
 sudo pacman -S --needed --noconfirm git nvidia wget firefox wl-clipboard man-db blueman bluez bluez-utils reflector pacman-contrib pavucontrol alsa-utils neofetch gtk4
-
 reflector --sort rate --country jp --latest 10 --save /etc/pacman.d/mirrorlist
 
 # wf-install dependencies
 sudo pacman -S --needed --noconfirm glm meson cmake seatd
 
-# waybar dependencies
+# waybar with the dependencies
 sudo pacman -S --needed --noconfirm waybar mpd ttf-font-awesome
 
-# add display manager here
+# add dm here as needed
 sudo systemctl enable bluetooth systemd-timesyncd systemd-networkd systemd-resolved paccache.timer --now
 
 git clone https://aur.archlinux.org/yay-bin.git &&
@@ -22,29 +20,28 @@ cd yay-bin &&
 makepkg -si
 
 yay -S --noconfirm google-chrome archlinux-themes-sddm ttf-menlo-powerline-git rofi-lbonn-wayland
-
 fc-cache -vf
-
-# conf rewrite
-sudo sed -i -e "/^ *#Current=$/c\ Current=archlinux-simplyblack" /usr/lib/sddm/sddm.conf.d/default.conf
-sudo sed -i -e "/^ *#DefaultTimeoutStartSec=/c\ DefaultTimeoutStartSec=10s" /etc/systemd/system.conf
-sudo sed -i -e "/^ *#DefaultTimeoutStopSec=/c\ DefaultTimeoutStopSec=10s" /etc/systemd/system.conf
-sudo sed -i -e "/^ *#Color$/c\ Color\n\ ILoveCandy" /etc/pacman.conf
 
 git clone https://github.com/WayfireWM/wf-install &&
 cd wf-install &&
 ./install.sh --prefix /opt/wayfire --stream master
 
+# rewrite
+sudo sed -i -e "/^ *#Current=$/c\ Current=archlinux-simplyblack" /usr/lib/sddm/sddm.conf.d/default.conf
+sudo sed -i -e "/^ *#DefaultTimeoutStartSec=/c\ DefaultTimeoutStartSec=10s" /etc/systemd/system.conf
+sudo sed -i -e "/^ *#DefaultTimeoutStopSec=/c\ DefaultTimeoutStopSec=10s" /etc/systemd/system.conf
+sudo sed -i -e "/^ *#Color$/c\ Color\n\ ILoveCandy" /etc/pacman.conf
+
+# append
 env_lines="GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx"
 nw_lines="IPv6PrivacyExtensions=true\nIgnoreCarrierLoss=3s"
-
 sudo echo -e "$env_lines" | sudo tee -a /etc/environment
 sudo echo -e "$nw_lines" | sudo tee -a /etc/systemd/network/10-wlan0.network
+
+# software cursor with nvidia gpu
+sc_lines="WLR_NO_HARDWARE_CURSORS=1"
+sudo echo -e "$sc_lines" | sudo tee -a /etc/environment
 
 # also available in .config/chrome-flags.conf
 #chrome_flags="--force-dark-mode --enable-features=WebUIDarkMode --ozone-platform-hint=auto --enable-features=TouchpadOverscrollHistoryNavigation --gtk-version=4"
 #sudo echo "$chrome_flags" | sudo tee -a /opt/google/chrome/google-chrome
-
-# wayfire software cursor
-sc_lines="WLR_NO_HARDWARE_CURSORS=1"
-sudo echo -e "$sc_lines" | sudo tee -a /etc/environment
