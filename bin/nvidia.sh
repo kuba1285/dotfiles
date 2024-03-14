ebddad2d560efde315538e9c1b38df55104e48f3
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-14 11:32:07 +0900

    Delete bin/nvidia.sh

--- bin/nvidia.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# update config
-sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
-echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
-echo -e "WLR_NO_HARDWARE_CURSORS=1" | sudo tee -a /etc/environment
