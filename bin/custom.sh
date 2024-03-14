f9492dc9d7e758afdd0d5503c3faf1730fa149b9
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-14 12:44:19 +0900

    Update custom.sh

--- bin/custom.sh
+++ bin/custom.sh
@@ -9,7 +9,6 @@ tar xvzf v1.4.0.tar.gz
 cd 3d-ascii-viewer*
 make
 find ./models -name "*.mtl" -type f | xargs rm
-mv 3d-ascii-viewer models $HOME/bin/.
-cd
-rm v1.4.0.tar.gz
-rm -rf 3d-ascii-viewer*
+mv 3d-ascii-viewer models/ $HOME/bin/.
+cd 
+rm -rf 3d-ascii-viewer* v1.4.0.tar.gz
