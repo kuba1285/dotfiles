648d497599bd29a03a92e14caa3cb32ca503113a
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-14 11:31:55 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -123,7 +123,11 @@ fi
 # Setup Nvidia if it was found
 if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia ; then
     install_list $LISTNVIDIA
-    source $BIN/nvidia.sh
+    # update config
+    sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+    sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
+    echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
+    echo -e "WLR_NO_HARDWARE_CURSORS=1" | sudo tee -a /etc/environment
 fi
 
 # Install custom app
