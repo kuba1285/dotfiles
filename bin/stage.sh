b0e762557038e98e495d7334fe427c3f539f5195
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-15 20:30:40 +0900

    Update stage.sh

--- bin/stage.sh
+++ bin/stage.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-{{ if eq .chezmoi.os "android" }}
-
-chmod +x ~/.config/polybar/scripts/*
-yay -R --noconfirm xfdesktop xfwm4-themes &>> $INSTLOG
-
-{{ end }}
 
 {{ if eq .chezmoi.os "linux" }}
 
 chmod +x ~/.config/polybar/scripts/*
 chmod +x ~/.config/hypr/scripts/*
+yay -R --noconfirm xfdesktop xfwm4-themes &>> $INSTLOG
 
 # add VScode extensions
 mkdir ~/.vscode
