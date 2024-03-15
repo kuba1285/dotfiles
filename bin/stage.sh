dc22f2a88e7f75de21f57b8b934befd9ec926e3e
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-15 15:34:59 +0900

    Update stage.sh

--- bin/stage.sh
+++ bin/stage.sh
@@ -4,6 +4,7 @@ chmod +x ~/.config/polybar/scripts/*
 yay -R --noconfirm xfdesktop xfwm4-themes &>> $INSTLOG
 
 {{ if eq .chezmoi.os "linux" }}
+
 chmod +x ~/.config/polybar/scripts/*
 chmod +x ~/.config/hypr/scripts/*
 
@@ -16,4 +17,5 @@ sudo tar -xf $PARENT/src/sugar-candy.tar.gz -C /usr/share/sddm/themes/
 sudo chown -R $USER:$USER /usr/share/sddm/themes/sugar-candy
 sudo mkdir /etc/sddm.conf.d
 echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee -a /etc/sddm.conf.d/10-theme.conf
+
 {{ end }}
