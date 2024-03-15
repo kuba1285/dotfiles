d5f1588d6773dc28ace06adec6f9bddf83eab805
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-15 18:15:43 +0900

    Update termux-init.sh

--- bin/termux-init.sh
+++ bin/termux-init.sh
@@ -1,7 +1,7 @@
 cd
 termux-setup-storage
 yes | pkg upgrade
-yes | pkg install termux-api termux-exec fakeroot git curl which proot proot-distro pulseaudio x11-repo
+yes | pkg install game-repo science-repo root-repo x11-repo termux-api termux-exec fakeroot git curl which proot proot-distro pulseaudio
 proot-distro install archlinux
 
 echo "sleep 3 && proot-distro login archlinux --user root --shared-tmp" >> $HOME/.bashrc
