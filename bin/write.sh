2f43fb3b77431c90fba9c55f7f8cf4da80722feb
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-15 15:53:30 +0900

    Update write.sh

--- bin/write.sh
+++ bin/write.sh
@@ -20,6 +20,9 @@ EOF
 # yabai sudoers setting
 echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
 
+source $BIN/parse-plist
+show_progress $!
+
 # Enable services
 yabai --start-service
 skhd --start-service
