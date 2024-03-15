cca4c5ba6cd1fff1329a42f76f4dfab08a7bd95c
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-15 14:38:23 +0900

    Update stage.sh

--- bin/stage.sh
+++ bin/stage.sh
@@ -12,6 +12,3 @@ sudo tar -xf $PARENT/src/sugar-candy.tar.gz -C /usr/share/sddm/themes/
 sudo chown -R $USER:$USER /usr/share/sddm/themes/sugar-candy
 sudo mkdir /etc/sddm.conf.d
 echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee -a /etc/sddm.conf.d/10-theme.conf
-
-# Clean out other portals
-yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk &>> $INSTLOG
