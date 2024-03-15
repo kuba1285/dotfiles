f7b9093a0c51fcd25344637e9d9f7b6cba687061
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-15 16:16:40 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -17,7 +17,8 @@ SERVICES=(
 
 function set_colors() {
     if [ -t 1 ] ; then
-        RED=$(tput setaf 1); GREEN=$(tput setaf 2); YELLOW=$(tput setaf 3); CYAN=$(tput setaf 6); BOLD=$(tput bold); RESET=$(tput sgr0)
+        RED=$(tput setaf 1); GREEN=$(tput setaf 2); YELLOW=$(tput setaf 3);
+        CYAN=$(tput setaf 6); BOLD=$(tput bold); RESET=$(tput sgr0)
     else
         RED=""; GREEN=""; YELLOW=""; CYAN=""; BOLD=""; RESET=""
     fi
