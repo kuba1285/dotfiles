9c14293d40b1dc8522a9086b427eb92aa8c4582b
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-16 03:49:49 +0900

    Update stage.sh

--- bin/stage.sh
+++ bin/stage.sh
@@ -13,5 +13,3 @@ sudo tar -xf $PARENT/src/sugar-candy.tar.gz -C /usr/share/sddm/themes/
 sudo chown -R $USER:$USER /usr/share/sddm/themes/sugar-candy
 sudo mkdir /etc/sddm.conf.d
 echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee -a /etc/sddm.conf.d/10-theme.conf
-
-{{ end }}
