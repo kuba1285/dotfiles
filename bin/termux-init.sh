9d8ad7262491c9afed27d35639f8c0018af7bd25
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-15 19:27:45 +0900

    Update termux-init.sh

--- bin/termux-init.sh
+++ bin/termux-init.sh
@@ -3,7 +3,7 @@ termux-setup-storage
 yes | pkg install termux-keyring termux-api tsu termux-exec fakeroot git curl which proot proot-distro pulseaudio termux-x11-nightly
 proot-distro install archlinux
 
-echo "sleep 3 && proot-distro login archlinux --user user --shared-tmp" >> $HOME/.bashrc
+echo "sleep 3 && proot-distro login archlinux --user root --shared-tmp" >> $HOME/.bashrc
 touch .hushlogin
 
 mkdir -p ~/.shortcuts
