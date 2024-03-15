59ec196dffa91e0743f965c74c09b6c3adc0beb0
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-15 14:39:35 +0900

    Update stage.sh

--- bin/stage.sh
+++ bin/stage.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 chmod +x ~/.config/polybar/scripts/*
+
+{{ if eq .chezmoi.os "linux" }}
 chmod +x ~/.config/hypr/scripts/*
 
 # add VScode extensions
@@ -12,3 +14,4 @@ sudo tar -xf $PARENT/src/sugar-candy.tar.gz -C /usr/share/sddm/themes/
 sudo chown -R $USER:$USER /usr/share/sddm/themes/sugar-candy
 sudo mkdir /etc/sddm.conf.d
 echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee -a /etc/sddm.conf.d/10-theme.conf
+{{ end }}
