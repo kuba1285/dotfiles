656315edc5bd121e57c08be24bea943a6aa5c09f
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-15 14:04:56 +0900

    Update write.sh

--- bin/write.sh
+++ bin/write.sh
@@ -6,6 +6,12 @@ bash $HOME/bin/change-wallpaper.sh
 neowofetch --gap -30 --ascii \$(fortune -s | pokemonsay -w 30)"
 EOF
 
+{{ if eq .chezmoi.os "android" }}
+cat << EOF >> ~/.bashrc
+fortune | pokemonsay
+neofetch --off
+EOF
+
 # yabai sudoers setting
 echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
 {{ end }}
