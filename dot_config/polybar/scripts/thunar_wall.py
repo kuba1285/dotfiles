8566062ba3f7d991e3d3bc83175cd848ac3910e3
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-17 02:11:51 +0900

    Update thunar_wall.py

--- dot_config/polybar/scripts/thunar_wall.py
+++ dot_config/polybar/scripts/thunar_wall.py
@@ -15,21 +15,13 @@ image_path = args["image"]
     os.system(f'cp {image_path} ~/.config/swww/wallpaper.rofi')
     # cava
     os.system(f'cp ~/.cache/wal/colors-cava ~/.config/cava/config')  
-    # hyprland
-    os.system(f'cp ~/.cache/wal/colors-hyprland.conf ~/.config/hypr/colors-hyprland.conf')
-    # waybar
-    os.system(f'cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/')  
     # kitty 
     os.system(f'cp ~/.cache/wal/colors-kitty.conf ~/.config/kitty/')
-    # swaylock
-    os.system(f'cp ~/.cache/wal/colors-swaylock ~/.config/swaylock/config')
     # mako
     os.system(f'cp ~/.cache/wal/colors-mako ~/.config/mako/config')
-     # rofi 
+    # rofi 
     os.system(f'cp ~/.cache/wal/colors-rofi.rasi ~/.config/rofi/colors/colors-rofi.rasi')  
-     # firefox
+    # firefox
     os.system(f'pywalfox update')
-    # Reload waybar to apply colorscheme
-    os.system(f'killall -SIGUSR2 waybar') 
     # Send a notification
     os.system(f'notify-send -h string:x-canonical-private-synchronous:hypr-cfg -u low "Wallpaper changed to {random_image}"')
