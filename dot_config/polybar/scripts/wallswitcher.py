b9580193593f61eae9a7286d7501fa5a9e9c59e3
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-17 02:16:13 +0900

    Add files via upload

--- dot_config/polybar/scripts/wallswitcher.py
+++ dot_config/polybar/scripts/wallswitcher.py
@@ -1,14 +1,28 @@
+#!/usr/bin/env python
+
 import os
-import argparse
+import random
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to input image")
-args = vars(ap.parse_args())
+# full path to your wallpaper folder
+wallpath = os.path.expanduser("~/.config/swww")
 
-image_path = args["image"]
+
+def change_wallpaper(folder_path):
+    # Get a list of image files in the specified folder
+    image_files = [f for f in os.listdir(folder_path) if f.lower().endswith(
+        ('.png', '.jpg', '.jpeg', '.gif'))]
+
+    if not image_files:
+        os.system(
+            f'notify-send -h string:x-canonical-private-synchronous:hypr-cfg -u low "No image files found in the folder"')
+        return
+
+    # Choose a random image from the list
+    random_image = random.choice(image_files)
+    image_path = os.path.join(folder_path, random_image)
 
     # Change the wallpaper
-    os.system(f'swww img -t wipe --transition-duration 3 {image_path}')
+    os.system(f'swww img -t outer --transition-duration 3 {image_path}')
     # Generarte Pywal colorscheme
     os.system(f'wal -i {image_path}')
     #generate wallpaper.rofi 
@@ -32,4 +46,10 @@ image_path = args["image"]
     # Reload waybar to apply colorscheme
     os.system(f'killall -SIGUSR2 waybar') 
     # Send a notification
-    os.system(f'notify-send -h string:x-canonical-private-synchronous:hypr-cfg -u low "Wallpaper changed to {random_image}"')
+    os.system(
+        f'notify-send -h string:x-canonical-private-synchronous:hypr-cfg -u low "Wallpaper changed to {random_image}"')
+
+
+if __name__ == "__main__":
+    folder_path = wallpath
+    change_wallpaper(folder_path)
