7dbb582355c409776a01cd81078746fe19971471
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-16 23:52:01 +0900

    Update stage.sh

--- bin/stage.sh
+++ bin/stage.sh
@@ -6,10 +6,10 @@ yay -R --noconfirm xfdesktop xfwm4-themes
 
 # add VScode extensions
 mkdir ~/.vscode
-tar -xf $PARENT/src/extensions.tar.gz -C ~/.vscode/
+tar -xf $HOME/src/extensions.tar.gz -C ~/.vscode/
 
 # Copy the SDDM theme
-sudo tar -xf $PARENT/src/sugar-candy.tar.gz -C /usr/share/sddm/themes/
+sudo tar -xf $HOME/src/sugar-candy.tar.gz -C /usr/share/sddm/themes/
 sudo chown -R $USER:$USER /usr/share/sddm/themes/sugar-candy
 sudo mkdir /etc/sddm.conf.d
 echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee -a /etc/sddm.conf.d/10-theme.conf
