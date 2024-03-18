0a1b4c1f51af5153bd34559a2e78743188265fad
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-18 20:31:13 +0900

    Update termux-init.sh

--- bin/termux-init.sh
+++ bin/termux-init.sh
@@ -3,7 +3,7 @@ termux-setup-storage
 yes | pkg install termux-keyring termux-api tsu termux-exec fakeroot git curl which proot proot-distro pulseaudio termux-x11-nightly
 proot-distro install archlinux
 
-echo　<< EOF >> $HOME/.bashrc
+cat << EOF >> $HOME/.bashrc
 proot-distro login archlinux
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
