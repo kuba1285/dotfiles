bf8f27cbeb901272f6301a96491ba0497c78fd16
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-18 21:02:17 +0900

    Update termux-init.sh

--- bin/termux-init.sh
+++ bin/termux-init.sh
@@ -1,12 +1,13 @@
 cd
 termux-setup-storage
-yes | pkg install termux-keyring termux-api tsu termux-exec fakeroot git curl which proot proot-distro pulseaudio termux-x11-nightly
+yes | pkg install termux-keyring termux-api tsu termux-exec fakeroot git curl which proot proot-distro pulseaudio termux-x11-nightly virglrenderer-android
 proot-distro install archlinux
 
 cat << EOF >> $HOME/.bashrc
-proot-distro login archlinux
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
+virgl_test_server_android &
+proot-distro login archlinux
 EOF
 
 cat << EOF >> $HOME/.termux/termux.properties
