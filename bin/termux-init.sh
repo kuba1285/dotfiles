4eda7505ce5cece24f231552c2477405272ab7a8
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-18 17:56:49 +0900

    Update termux-init.sh

--- bin/termux-init.sh
+++ bin/termux-init.sh
@@ -1,25 +1,10 @@
 cd
 termux-setup-storage
-yes | pkg install termux-keyring termux-api tsu termux-exec fakeroot git curl which proot proot-distro pulseaudio termux-x11-nightly virglrenderer-android
+yes | pkg install termux-keyring termux-api tsu termux-exec fakeroot git curl which proot proot-distro pulseaudio termux-x11-nightly
 proot-distro install archlinux
 
-echo "sleep 3 && proot-distro login archlinux --user root --shared-tmp" >> $HOME/.bashrc
+echo "proot-distro login archlinux" >> $HOME/.bashrc
 touch .hushlogin
-
-mkdir -p ~/.shortcuts
-cat << EOF > ~/.shortcuts/TermuxGUI
-#!/bin/bash
-killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-lock
-am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
-XDG_RUNTIME_DIR=${TMPDIR}
-termux-x11 :0 -ac &
-sleep 3
-pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
-pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
-virgl_test_server_android &
-proot-distro login archlinux --user root --shared-tmp -- bash -c "export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session startxfce4"
-EOF
-chmod +x ~/.shortcuts/TermuxGUI
 
 cat << EOF >> $HOME/.termux/termux.properties
 extra-keys = [ \\
