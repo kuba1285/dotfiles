3572cd6a17fbab9779e7427d262035d953464adc
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-22 21:41:29 +0900

    Add files via upload

---
+++ bin/iconify-png.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n "Name of Iconify PNG(without extension)?:"
+read filename
+
+array=(16 32 128 256 512)
+
+mkdir "${filename}.iconset"
+for i in "${array[@]}"
+do
+    sips -Z $i ${filename}.png --out "${filename}.iconset/icon_${i}x${i}.png"
+    sips -Z $((i*2)) ${filename}.png --out "${filename}.iconset/icon_${i}x${i}@2x.png"
+done
+
+iconutil -c icns "${filename}.iconset"
+rm -rf "${filename}.iconset"
