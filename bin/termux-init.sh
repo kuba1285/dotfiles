7c0041e6d287940c7907df43f150754fec9d2c1d
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-22 19:09:14 +0900

    Update termux-init.sh

--- bin/termux-init.sh
+++ bin/termux-init.sh
@@ -9,7 +9,7 @@ cat << EOF > $HOME/.bashrc
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 virgl_test_server_android &
-proot-distro login archlinux
+proot-distro login archlinux --user user
 EOF
 
 cat << EOF > $HOME/.termux/termux.properties
