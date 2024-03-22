e15e9adaa6042d417302d0a30f5bb308eb031939
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-22 17:39:23 +0900

    Update termux-init.sh

--- bin/termux-init.sh
+++ bin/termux-init.sh
@@ -5,11 +5,50 @@ proot-distro install archlinux
 
 touch ~/.hushlogin
 
+cat << EOF > ~/startarch.sh
+#!/data/data/com.termux/files/usr/bin/bash
+
+cd \$(dirname \$0)
+## unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+
+# proot
+command="proot"
+command+=" --sysvipc --link2symlink -0"
+
+# root directory folder
+command+=" -r /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux"
+
+# specify some directories
+command+=" -b /dev -b /proc "
+
+# /dev/shm is a directory in memory (≈memory disk), which speeds up reading and writing, and does not need to be set.
+command+=" -b /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux/root:/dev/shm"
+
+# Optional
+#command+=" -b \$ANDROID_DATA"
+command+=" -b \$EXTERNAL_STORAGE"
+command+=" -b \$HOME"
+command+=" -b /linkerconfig/ld.config.txt"
+
+# It is best not to change the following
+command+=" -w /root /usr/bin/env"
+command+=" -i HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=\$TERM"
+command+=" LANG=en_US.UTF-8"
+command+=" /bin/bash --login"
+
+exec \$command
+EOF
+
+chmod +x ~/startarch.sh
+
 cat << EOF > $HOME/.bashrc
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 virgl_test_server_android &
-proot-distro login archlinux
+~/startarch.sh
 EOF
 
 cat << EOF > $HOME/.termux/termux.properties
