002b084da15ab644712a1943028a991d042a2d6c
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-23 14:27:13 +0000

    Add files via upload

---
+++ Scripts/termux-init.sh
@@ -0,0 +1,24 @@
+cd
+termux-setup-storage
+yes | pkg install termux-keyring termux-api termux-exec git curl which proot proot-distro pulseaudio termux-x11-nightly virglrenderer-android
+proot-distro install archlinux
+
+touch ~/.hushlogin
+
+cat << EOF > $HOME/.bashrc
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
+export DISPLAY=:0
+termux-x11 :0 &
+virgl_test_server_android &
+proot-distro login archlinux --shared-tmp
+EOF
+
+cat << EOF > $HOME/.termux/termux.properties
+extra-keys = [ \\
+		 ['ESC','|', '/', '~','HOME','UP','END'], \\
+		 ['TAB', 'CTRL', '=', '-','LEFT','DOWN','RIGHT'] \\
+		]
+EOF
+termux-reload-settings
+exit
