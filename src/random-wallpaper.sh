bd6ce9ca577bf6b46bce15bbe5009ef7c21b0074
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-03-24 09:05:46 +0000

    Add files via upload

---
+++ src/random-wallpaper.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+
+ # Get a random image file from the background image folder.
+ IMAGE_FILE=$(find /Pictures/ -type f | shuf -n 1)
+ 
+ # Get the path to the sddm backgrounds folder.
+ SDDM_BACKGROUNDS_FOLDER="/usr/share/sddm/themes/sugar-candy/Backgrounds"
+ 
+ # Create a symbolic link to the random image file in the sddm backgrounds folder (overwrite with the force flag).
+ ln -sf "$IMAGE_FILE" "$SDDM_BACKGROUNDS_FOLDER/randomlySelectedWallpaper"
