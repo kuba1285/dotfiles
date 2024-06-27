e71664b0cc5f15fdacb776346aeba6537f44b68a
kuba1285 <58022676+kuba1285@users.noreply.github.com>
2024-06-27 19:39:21 +0900

    Update termux-init.sh

--- Scripts/termux-init.sh
+++ Scripts/termux-init.sh
@@ -5,14 +5,16 @@ proot-distro install archlinux
 
 touch ~/.hushlogin
 
+#cat << EOF > $HOME/.bashrc
+#pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+#pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
+#export DISPLAY=:0
+#termux-x11 :0 &
+#virgl_test_server_android &
+#am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity &&
+#proot-distro login archlinux --shared-tmp ||
+
 cat << EOF > $HOME/.bashrc
-pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
-pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
-export DISPLAY=:0
-termux-x11 :0 &
-virgl_test_server_android &
-am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity &&
-proot-distro login archlinux --shared-tmp ||
 proot-distro login archlinux --shared-tmp
 EOF
 
